c1175ba798ff8aa91ff1f35dcebf0d9fb311fd21
AlexHorbenko <agorbenko1501@gmail.com>
2022-11-10 16:30:36 +0200

    Completed

---
+++ script.js
@@ -0,0 +1,25 @@
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+// removeElement(array, 5);
+// Результат: [1, 2, 3, 4, 6, 7]
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+// ==========================================Splice========================================
+const arrayNew = array.splice(4, 1)
+console.log(array);
+
+// =========================================For + Splice===================================
+for( let i = 0; i < array.length; i++){ 
+    
+    if ( array[i] === 5) { 
+
+        array.splice(i, 1); 
+    }
+};
+console.log(array);
+
+// ==========================================Filter========================================
+const newArray = array.filter(function(number) {
+    return number !== 5;
+});
+console.log(newArray);
